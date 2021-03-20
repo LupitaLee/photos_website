efe37578c3598367b21db52ec9bd4ddf81f311ba
Lupita <lupitarivera8899@gmail.com>
2021-03-20 17:13:22 -0500

    added gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+
+
 require "bundler/setup"Bundler.require
 ActiveRecord::Base.establish_connection({  
       :adapter => "sqlite3",    
       :database => "db/development.sqlite3"})
 require_all 'app'
+
+
+require './app/controllers/application_controller'
